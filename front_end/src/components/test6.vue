3351172a7c09d29f073859742d65a6b5e43a5ca5
Dingkun_Liu <91831067+Dingkun0817@users.noreply.github.com>
2023-09-05 15:21:04 +0800

    Update test6.vue

--- front_end/src/components/test6.vue
+++ front_end/src/components/test6.vue
@@ -1,42 +1,44 @@
 <template>
-    <div class="app">
-      <h1>模型选择和图片上传</h1>
-      <button @click="toggleModelSelection">模型选择</button>
-  
-      <!-- 显示模型选择按钮组 -->
-      <div v-if="showModelSelection" class="model-buttons">
-        <button v-on:click="runModel(1), upload()">运行SAM模型</button>
-        <!-- <button @click="runModel(2)">运行模型2</button>
-        <button @click="runModel(3)">运行模型3</button> -->
+  <div id="first-page">
+    <div class="left">
+      <div class="app">
+        <h1>模型选择和图片上传</h1>
+        <button @click="toggleModelSelection">模型选择</button>
+    
+        <!-- 显示模型选择按钮组 -->
+        <div v-if="showModelSelection" class="model-buttons">
+          <button v-on:click="runModel(1), upload()">运行SAM模型</button>
+          <!-- <button @click="runModel(2)">运行模型2</button>
+          <button @click="runModel(3)">运行模型3</button> -->
+        </div>
+    
+        <!-- 文件上传输入框 -->
+        <input type="file" ref="fileInput" style="display: none" @change="uploadImage" accept="image/*">
+        <button v-on:click="openFileInput()">上传图片</button>
+    
+        <!-- 上传进度显示 -->
+        <div v-if="uploading" class="upload-progress">
+          <p>上传进度：</p>
+          <progress :value="uploadProgress" max="100"></progress>
+        </div>
+    
+        <!-- 上传完成后显示图片 -->
+        <div v-if="uploadedImage" class="uploaded-image">
+          <p>上传完成的图片：</p>
+          <img :src="uploadedImage" alt="上传的图片">
+        </div>
       </div>
-  
-      <!-- 文件上传输入框 -->
-      <input type="file" ref="fileInput" style="display: none" @change="uploadImage" accept="image/*">
-      <button v-on:click="openFileInput()">上传图片</button>
-  
-      <!-- 上传进度显示 -->
-      <div v-if="uploading" class="upload-progress">
-        <p>上传进度：</p>
-        <progress :value="uploadProgress" max="100"></progress>
-      </div>
-  
-      <!-- 上传完成后显示图片 -->
-      <div v-if="uploadedImage" class="uploaded-image">
-        <p>上传完成的图片：</p>
-        <img :src="uploadedImage" alt="上传的图片">
-      </div>
-
-
-      
-      <div v-if="imageUrl">
+    </div>
+    <div class="right">
+      <div v-if="imageUrl" class = "result-display">
         <!-- <img src="" alt="hhhhhhhhhhhhhhhhhh"> -->
-        {{ imageUrl }}
+        <!-- {{ imageUrl }} -->
+        <!-- {{ res.data['message'] }} -->
         <img src="../assets/result/1.png" alt="hhhhhhhhhhhhhhhhhh">
       </div>
-      
-
-
     </div>
+  </div>
+
   </template>
   
   <script>
@@ -140,11 +142,38 @@
   </script>
   
   <style scoped>
+
+#first-page{
+    background-image: linear-gradient(
+      90deg,
+      cyan,
+      purple
+    );
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-size: 400%;
+    animation: myanimation 10s infinite;
+  }
+@keyframes myanimation {
+  0%{
+    background-position: 0% 50%;
+  }
+  50%{
+    background-position: 100% 50%;
+  }
+  100%{
+    background-position: 0% 50%;
+  }
+}
   .app {
     text-align: center;
-    margin: 20px;
+    margin: 200px;
+    margin-top: 10px;
   }
-  
+
   button {
     margin: 10px;
     padding: 10px 20px;
@@ -177,6 +206,18 @@
   img {
     max-width: 100%;
   }
+
+  .left {
+  width: 60%;
+  float: left;
+}
+
+.right {
+  width: 40%;
+  float: right;
+  /* right: 50%;
+  margin-left: 400px; */
+}
   </style>
   
   
@@ -286,7 +327,7 @@
     }
   
     .upload-progress {
-      margin-top: 20px;
+      margin-top: 20px;·
     }
   
     .uploaded-image {
